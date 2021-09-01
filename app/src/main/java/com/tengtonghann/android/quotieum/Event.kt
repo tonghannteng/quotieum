b4e84b8b3eb928a86d11b3026a9bfc98f2a3d1fb
Tonghann Teng <tengtonghann@gmail.com>
2021-08-31 20:22:28 -0500

    Add Event

---
+++ app/src/main/java/com/tengtonghann/android/quotieum/Event.kt
@@ -0,0 +1,22 @@
+package com.tengtonghann.android.quotieum
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * @author Tonghann Teng
+ * Used as a wrapper for data that is exposed via a LiveData that represents as event.
+ */
+data class Event<out T>(private val content: T) {
+
+    private var hasBeenHandled = AtomicBoolean(false)
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getIfNotHandled(): T? = if (hasBeenHandled.getAndSet(true)) null else content
+
+    /**
+     * Returns the content, even if is is already been handled
+     */
+    fun peek(): T = content
+}
---
+++ app/src/main/java/com/tengtonghann/android/quotieum/data/Data.kt
@@ -0,0 +1,10 @@
+package com.tengtonghann.android.quotieum.data
+
+data class Data(
+    val _vv: Int,
+    val _id: String,
+    val quoteAuthor: String,
+    val quoteGenre: String,
+    val quoteText: String
+)
+// performance, improvement
